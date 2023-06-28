408a01c6466e2742b6b093c686591fc2a6cc9a27
fwehn <finn.wehn@web.de>
2023-06-29 00:07:25 +0200

    listing sentences on SkillPage.vue;

--- src/src/components/SkillPage.vue
+++ src/src/components/SkillPage.vue
@@ -18,28 +18,47 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 <script>
 import { activateSkill, deleteSkill, getSkillDetails, postOptions } from "@/services/APIService";
 import LoadingBar from "@/components/LoadingBar.vue";
+import SlotComponent from "@/components/SlotComponent.vue";
 
 export default {
     name: "SkillPage",
-    components: { LoadingBar },
+    components: { SlotComponent, LoadingBar },
     data() {
         return {
             skillName: this.$route.params["skill"],
             description: "",
             version: "1.0",
+            sentences: [],
             active: false,
             options: {},
             loading: false,
-            loadingMessage: ""
+            loadingMessage: "",
+            slots: [],
+            slotsValues: []
         };
     },
     methods: {
         loadPage() {
             getSkillDetails(this.skillName)
                 .then((response) => {
-                    console.log(response);
                     this.description = response["description"];
                     this.version = response["version"];
+
+                    let slotsValues = [];
+                    let sentences = [];
+
+                    response["intents"].forEach((intent) => {
+                        sentences.push(...intent["sentences"]);
+                    });
+
+                    this.sentences = sentences.map((sentence, index) => {
+                        slotsValues[index] = [...sentence.matchAll(/\((.*?)\)(?:\{(\b\w*\b)})?/g)];
+                        return sentence.replaceAll(/\((.*?)\)(?:\{(\b\w*\b)})?/g, "#");
+                    });
+
+                    this.slotsValues = slotsValues;
+                    this.slots = response["slots"];
+
                     this.options = response["options"];
                     this.active = response["active"];
                 })
@@ -63,6 +82,37 @@ export default {
         deleteSkill() {
             this.loading = true;
             deleteSkill(this.skillName).finally(() => this.$router.replace({ path: "/skill" }));
+        },
+        getSlot(x, y) {
+            const currentSlot = this.slotsValues[x][y];
+            const slotValues = currentSlot[1];
+
+            const specialChar = slotValues.replaceAll(/\s+/g, "").match(/.*(\W).*/);
+
+            let type, alternatives;
+
+            switch (specialChar ? specialChar[1] : "") {
+                case "|":
+                    type = "nameless";
+                    alternatives = slotValues.split("|").map((alternative) => alternative.trim());
+                    break;
+
+                case ".":
+                    type = "range";
+                    alternatives = slotValues.split("..");
+                    break;
+
+                case "/":
+                    type = slotValues.split("$slots/")[1];
+                    alternatives = this.slots[type];
+                    break;
+
+                default:
+                    type = "undefined";
+                    alternatives = [];
+            }
+
+            return { type: type, alternatives: alternatives };
         }
     },
     mounted() {
@@ -78,6 +128,30 @@ export default {
         <h1 class="text-4xl w-1/2 text-center">{{ skillName }}</h1>
         <h2 class="text-2xl w-1/2 text-center">{{ version }}</h2>
         <h3 class="text-xl w-1/2 text-center" v-html="description"></h3>
+
+        <div class="w-1/2 border-b-2 border-b-main-medium"></div>
+
+        <!--        <div>{{ slotsValues }}</div>-->
+
+        <div class="w-1/2 flex flex-col items-center gap-3">
+            <h2 class="text-2xl">{{ $t("skillPage.sentences") }}</h2>
+            <ul class="w-full">
+                <li v-for="(sentence, x) in sentences" v-bind:key="x">
+                    <p>
+                        ...
+                        <template v-for="(text, y) in sentence.split('#')" v-bind:key="y">
+                            {{ text }}
+
+                            <SlotComponent
+                                v-if="text && y < sentence.split('#').length && slotsValues[x] && slotsValues[x][y]"
+                                :slot-data="getSlot(x, y)"
+                                :slot-name="slotsValues[x][y][2] ? slotsValues[x][y][2] : ''"
+                            ></SlotComponent>
+                        </template>
+                    </p>
+                </li>
+            </ul>
+        </div>
 
         <div v-if="Object.keys(options).length !== 0" class="w-1/2 border-b-2 border-b-main-medium"></div>
 
---
+++ src/src/components/SlotComponent.vue
@@ -0,0 +1,59 @@
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "SlotComponent",
+    props: ["slotName", "slotData"],
+    data() {
+        const { type, alternatives } = this.slotData;
+        const sorted = alternatives.sort();
+
+        let example;
+        switch (type) {
+            case "number":
+                example = Math.floor(
+                    Math.random() * (Math.floor(sorted[1]) - Math.ceil(sorted[0]) + 1) + Math.ceil(sorted[0])
+                );
+                break;
+
+            default:
+                example = alternatives[Math.floor(Math.random() * alternatives.length)].split(":")[0];
+        }
+
+        return {
+            type: type,
+            alternatives: alternatives,
+            sorted: sorted,
+            example: example
+        };
+    }
+});
+</script>
+
+<template>
+    <div class="group relative inline-block text-accent-light duration-300">
+        {{ example }}
+
+        <div
+            class="group-hover:scale-100 scale-0 transition-all duration-75 origin-top flex flex-col gap-2 items-center absolute inset-x-1/2 -translate-x-1/2 w-56 max-h-44 p-1.5 z-50 overflow-y-scroll bg-main-medium text-center rounded-2xl border-2 border-main-light"
+        >
+            <h1 v-if="!['nameless', 'range'].includes(slotData['type'])" class="text-2xl underline">
+                {{ slotData["type"] }}
+            </h1>
+
+            <div v-if="slotData['type'] === 'range'" class="flex flex-row gap-5 w-full items-center justify-center">
+                <p class="w-1/5 text-end">{{ sorted[0] }}</p>
+                <p class="text-xl">⟾</p>
+                <p class="w-1/5 text-start">{{ sorted[1] }}</p>
+            </div>
+
+            <ul v-else>
+                <li v-for="(alternative, index) in alternatives" v-bind:key="index">
+                    {{ alternative.split(":")[0] }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
